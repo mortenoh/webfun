5a054c7a09bf5ae1580ae36a3d435c31927d6fa2
Morten Olav Hansen <mortenoh@gmail.com>
2009-10-07 17:15:30 +0200

    added count_xml/json

--- django/ajaxfun/students/urls.py
+++ django/ajaxfun/students/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('ajaxfun.students.views',
 	(r'^/$', 'index'),
-	(r'^/count/$', 'count'),
+	(r'^/count/plaintext/$', 'count_plaintext'),
+	(r'^/count/json/$', 'count_json'),
+	(r'^/count/xml/$', 'count_xml'),
 )
 
--- django/ajaxfun/students/views.py
+++ django/ajaxfun/students/views.py
@@ -1,12 +1,22 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+
 from models import Student
+import json
 
 def index(request):
 	return render_to_response('students/index.html', locals())
 
-def count(request):
+def count_plaintext(request):
 	n = Student.objects.all().count();
 	return HttpResponse(n, mimetype="text/plain")
+
+def count_json(request):
+	n = {'count' : Student.objects.all().count()}
+	return HttpResponse(json.dumps(n), mimetype="application/json")
+
+def count_xml(request):
+	n = '<count>%s</count>' % (Student.objects.all().count())
+	return HttpResponse(n, mimetype="application/xml")
 
